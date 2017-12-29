0cef5ea8d711522306c7a11389dcfd94b5d13ca2
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2017-12-29 22:34:00 +0100

    add first POJO

---
+++ src/main/java/model/Recordings.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.Date;
+
+public class Recordings {
+
+	int id;
+	String filename;
+	String url;
+	Date createdate;
+	int status;
+	int conversation;
+	String phone;
+	String parsedtext;
+	String misc;
+
+}
