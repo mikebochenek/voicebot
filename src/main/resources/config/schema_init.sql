0abcb14e27849c9ca00d883a021fb769081c4c97
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2018-01-02 21:08:24 +0100

    add restaurants table

--- src/main/resources/config/schema_init.sql
+++ src/main/resources/config/schema_init.sql
@@ -11,6 +11,7 @@ FLUSH PRIVILEGES;
    drop table users; 
    drop table recordings;
    drop table prompts;
+   drop table restaurants;
    */
 
 create table IF NOT EXISTS users (
@@ -54,6 +55,22 @@ create table IF NOT EXISTS prompts (
   primary key (id)
 );
 
+create table IF NOT EXISTS restaurants (
+  id INT NOT NULL AUTO_INCREMENT,
+  name varchar(255),
+  city varchar(45),
+  address varchar(255),
+  schedulecron varchar(1024),
+  createdate timestamp,
+  status int,
+  seats int,
+  email varchar(45),
+  phone varchar(45),
+  website varchar(255),
+  googleplaces varchar(35),
+  PRIMARY KEY (`id`)
+);
+
 
 INSERT INTO `voicebot`.`prompts` (`phone`, `ptext`, `actionurl`)
 VALUES ('+12262429755', 'Hello there cowboy, please leave a message to make a reservation.', '/voice/intro');
@@ -70,95 +87,4 @@ VALUES ('+12262429755', 'OK.', '/voice/processingwait');
 INSERT INTO `voicebot`.`prompts` (`phone`, `ptext`, `actionurl`)
 VALUES ('+12262429755', 'OK.  All done.  Thank you for calling.', '/voice/confirmation');
 
-
-create table IF NOT EXISTS polls (
-  uuid INT NOT NULL AUTO_INCREMENT,
-  id VARCHAR(32) NULL,
-  adminKey VARCHAR(32) NULL,
-  latestChange BIGINT NULL,
-  initiated BIGINT NULL,
-  participantsCount INT NULL,
-  inviteesCount INT NULL,
-  ttype VARCHAR(32) NULL, /* NOTE double T - type is probably reserved in some DBMS */
-  hidden BOOLEAN,
-  preferencesType VARCHAR(32) NULL,
-  sstate VARCHAR(32) NULL, /* NOTE double S - state is probably reserved in some DBMS */
-  locale VARCHAR(32) NULL,
-  title VARCHAR(1024) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL,
-  description VARCHAR(1024) CHARACTER SET utf8  NULL,
-  initiator_id INT NULL,
-  multiDate BOOLEAN,
-  device VARCHAR(32) NULL,
-  levels VARCHAR(32) NULL,
-  jsonfullpretty TEXT CHARACTER SET utf8   NULL,
-  PRIMARY KEY (`uuid`),
-  INDEX poll_title_idx (title),
-  INDEX poll_id_idx (id),
-  INDEX poll_initiated_idx (initiated)
-) ENGINE=InnoDB;
-
-/** poll_locations! */
-
-create table IF NOT EXISTS locations (
-  uuid INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(128) NULL,
-  address VARCHAR(1024) NULL,
-  countryCode VARCHAR(8) NULL,
-  locationId VARCHAR(32) NULL,
-  PRIMARY KEY (`uuid`),
-  INDEX location_id_idx (locationId)
-);
-
-
-create table IF NOT EXISTS poll_initiators (
-  uuid INT NOT NULL AUTO_INCREMENT,
-  poll_uuid INT NOT NULL,
-  initiator_uuid INT NOT NULL,
-  PRIMARY KEY (`uuid`),
-  INDEX pi_poll_uuid_idx (poll_uuid),
-  INDEX pi_initiator_uuid_idx (initiator_uuid)
-);
-
-create table IF NOT EXISTS initiators (
-  uuid INT NOT NULL AUTO_INCREMENT,
-  poll_uuid INT NOT NULL,
-  name VARCHAR(128) NULL,
-  email VARCHAR(128) NULL,
-  notify VARCHAR(8) NULL,
-  PRIMARY KEY (`uuid`),
-  INDEX initiator_email_idx (email),
-  INDEX initiator_poll_uuid_idx (poll_uuid)
-);
-
-create table IF NOT EXISTS participants (
-  uuid INT NOT NULL AUTO_INCREMENT,
-  id INT NULL,
-  name VARCHAR(128) NULL,
-  preferences VARCHAR(1024) NULL,
-  PRIMARY KEY (`uuid`)
-);
-
-create table IF NOT EXISTS poll_participants (
-  uuid INT NOT NULL AUTO_INCREMENT,
-  poll_uuid INT NOT NULL,
-  participant_uuid INT NOT NULL,
-  PRIMARY KEY (`uuid`),
-  INDEX pp_poll_uuid_idx (poll_uuid),
-  INDEX pp_participant_uuid_idx (participant_uuid)
-);
-  
-
-create table IF NOT EXISTS options (
-  uuid INT NOT NULL AUTO_INCREMENT,
-  poll_uuid INT NOT NULL,
-  start BIGINT NULL,
-  end BIGINT NULL,
-  allday BOOLEAN,
-  date BIGINT NULL,
-  startDate BIGINT NULL,
-  endDate BIGINT NULL,
-  text VARCHAR(128) NULL,
-  available VARCHAR(128) NULL,
-  PRIMARY KEY (`uuid`)
-);
 
