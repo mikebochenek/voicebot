ad05587d3634d821c920f9af6c0c1a251eefc1aa
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2018-01-02 14:21:19 +0100

    add more logging to FileDownload

--- src/main/java/util/FileDownload.java
+++ src/main/java/util/FileDownload.java
@@ -1,5 +1,9 @@
 package util;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
@@ -7,7 +11,10 @@ import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
 public class FileDownload {
+	public static Logger logger = LoggerFactory.getLogger(FileDownload.class);
+			
 	/**
+	 * download URL and save to a local file
 	 * https://stackoverflow.com/questions/921262/how-to-download-and-save-a-file-from-internet-using-java
 	 * @param url
 	 * @param filename
@@ -17,14 +24,18 @@ public class FileDownload {
 			URL website = new URL(url);
 			ReadableByteChannel rbc = Channels.newChannel(website.openStream());
 			FileOutputStream fos = new FileOutputStream(filename);
-			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+			long bytestrasferred = fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+			logger.info("downloaded: " + url + " - bytes: " + bytestrasferred);
 			fos.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error("failed download of: " + url, e);
 		}
 	}
 	
+	/**
+	 * create random filename for .wav files
+	 * @return
+	 */
 	public static String generateWAVFilename() {
 		String path = System.getProperty("data.local.path", "/tmp");
 		if (!path.endsWith("/")) {
