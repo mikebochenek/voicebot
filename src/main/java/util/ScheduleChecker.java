bdc74cd74f8abbdfd2dd202febfb2dc2e58ee80f
mike.bochenek@gmail.com <mike.bochenek@gmail.com>
2018-01-03 23:37:15 +0100

    add SchedulerChecker https://developers.google.com/places/web-service/details

---
+++ src/main/java/util/ScheduleChecker.java
@@ -0,0 +1,11 @@
+package util;
+
+public class ScheduleChecker {
+
+	/**
+	 * https://developers.google.com/places/web-service/details
+	 */
+	public static boolean isOpen(String time) {
+		return false;
+	}
+}
